50c43c670139bdcc7aae4cc10254b3853ccece7d
Sree Nihaar <115875131+SreeNihaar@users.noreply.github.com>
2024-05-16 15:44:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+let boxClick=0;
+
+let turnO= true;//playerX or playerO
+//true = playerO's turn
+//false = playerX's turn
+
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],    
+];
+
+const resetGame=()=>{
+    boxClick=0;
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const showWinner=(winner)=>{
+    disableBoxes();
+    msg.innerText=`Congratulations, Winner is Player ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const draw=()=>{
+    disableBoxes();
+    msg.innerText=`Draw`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner=()=>{
+    let winner=false;
+    for(let pattern of winPatterns){
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val=boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
+
+        if(pos1Val!=="" && pos2Val!=="" && pos3Val!==""){
+            if(pos1Val=== pos2Val && pos2Val===pos3Val){
+                winner=true;
+                showWinner(pos1Val);
+            }
+        }
+    }
+    return winner;
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(turnO){  //playerO
+            box.innerText="O";
+            box.style.color="#b0413e";
+            turnO=false;
+        }
+        else{   //playerX
+            box.innerText="X";
+            box.style.color="#3e82b0";
+            turnO=true;
+        }
+        box.disabled=true;
+        boxClick++;
+        let hasWinner=checkWinner();
+        if(boxClick===9 && !hasWinner){
+            draw();
+        }
+    });
+});
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
